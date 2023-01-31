7f127b6b52ce32e7080d6ea562ab58e85d28873b
d <xx@x.com>
2023-01-31 18:28:19 +0100

    Start

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,66 @@
-import "./style.css";
+// import "./style.css";
 
-const App = document.querySelector("#app");
+export default ((doc) => {
+  const getElement = (elm: string): Node => {
+    const defaultElm = doc.createElement("span");
+    return doc.querySelector(elm) || defaultElm;
+  };
 
-if (App) {
-  console.log("- ", App);
-  App.innerHTML = `
-  <div>
-  test
-  </div>
-`;
-}
+  const getElements = (elm: string): NodeList => {
+    return doc.querySelectorAll(elm);
+  };
+
+  const doArrows = (): void => {
+    const Right = getElement(".right");
+    const Left = getElement(".left");
+    const Slider = getElement(".slider") as HTMLElement;
+    const images = getElements(".image");
+
+    let slideNum = 1;
+    const slideWidth = 450;
+    const length = images.length;
+
+    const nextImage = () => {
+      Slider.style.transform = `translateX(-${slideNum * slideWidth}px)`;
+      slideNum++;
+    };
+
+    const firstImage = () => {
+      slideNum = 0;
+      Slider.style.transform = `translateX(${slideNum * slideWidth}px)`;
+      slideNum = 1;
+    };
+
+    const prevImage = () => {
+      Slider.style.transform = `translateX(-${(slideNum - 2) * slideWidth}px)`;
+      slideNum--;
+    };
+
+    const lastImage = () => {
+      slideNum = length - 1;
+      Slider.style.transform = `translateX(-${slideNum * slideWidth}px)`;
+      slideNum = length;
+    };
+
+    Right.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      console.log(slideNum);
+      if (slideNum === length) {
+        firstImage();
+      } else {
+        nextImage();
+      }
+    });
+
+    Left.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      if (slideNum === 1) {
+        lastImage();
+      } else {
+        prevImage();
+      }
+    });
+  };
+
+  doArrows();
+})(window.document);
